b23b745d5370d645e486719fe71a18e6e33e00c5
Nav <navdeepnx@gmail.com>
2023-11-19 12:05:05 -0800

    create user for signup added

--- index.js
+++ index.js
@@ -18,6 +18,15 @@ app.get('/', (req, res) => {
     res.send('Welcome to the Home Page');
 });
 
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.post('/login', (req, res) => {
+    res.send('Logged In (this is a placeholder response)');
+});
+
+
 app.get('/signup', (req, res) => {
     res.render('signup');
 });
@@ -26,14 +35,39 @@ app.post('/signup', (req, res) => {
     res.redirect('/users/login');
 });
 
+app.post('/createUser', async (req,res)=> { // Post Signup
 
-app.get('/login', (req, res) => {
-    res.render('login');
-});
-
-app.post('/login', (req, res) => {
-    res.send('Logged In (this is a placeholder response)');
-});
+    var username = req.body.username;
+    var email = req.body.email;
+    var password = req.body.password;
+  
+    var hashedPassword = bcrypt.hashSync(password, saltRounds);
+  
+    if (email && password && username) {
+        var success = await db_query.createUser({ username: username, email: email, hashed_pass: hashedPassword });
+  
+        if (success) {
+            res.redirect("/"); // User successfully created
+        }
+        else {
+            var createMsg = "Account already exists";
+            res.redirect(`/signup?createMsg=${createMsg}`)
+        }
+  
+    } else {
+        if(!username) {
+            var userMsg = "Please enter a username.";
+        }
+        if (!password) {
+            var passMsg = "Please enter a password.";
+        }
+        if (!email) {
+            var emailMsg = "Please enter an email.";
+        }
+        res.redirect(`/signup?userMsg=${userMsg}&emailMsg=${emailMsg}&passMsg=${passMsg}`)
+    }
+  });
+  
 
 
 const PORT = process.env.PORT || 3000;
