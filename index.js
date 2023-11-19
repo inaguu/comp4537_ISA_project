57978dabd9154f9a115f418c3661a3e25c878027
Nav <navdeepnx@gmail.com>
2023-11-19 11:44:07 -0800

    project structure created, base operation ready

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Home Page');
+});
+
+app.use('/users', userRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
