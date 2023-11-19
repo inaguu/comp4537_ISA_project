115431213e89fddc27cd0917cd88aacb22a589ad
Nav <navdeepnx@gmail.com>
2023-11-19 11:51:21 -0800

    routes added to login and signup

--- index.js
+++ index.js
@@ -8,6 +8,23 @@ app.get('/', (req, res) => {
     res.send('Welcome to the Home Page');
 });
 
+app.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
+app.post('/signup', (req, res) => {
+    res.redirect('/users/login');
+});
+
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.post('/login', (req, res) => {
+    res.send('Logged In (this is a placeholder response)');
+});
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
