cd98cde7417cfd08c53ada3323847e69c9f4849d
inaguu <ianjguani@gmail.com>
2023-11-19 23:03:30 -0800

    added CORS stuff

--- index.js
+++ index.js
@@ -12,6 +12,13 @@ const success = db_utils.printMySQLVersion();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET, PATCH, POST, DELETE, OPTIONS')
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
+    next()
+})
+
 app.post('/api/ISA/createuser', async (req,res)=> { // Post Signup
     let body = ""
         
